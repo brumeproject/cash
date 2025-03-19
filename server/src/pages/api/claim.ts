aaf9e24eaf890a481667d806ccf1d2d78e2bafff
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-03-19 22:54:19 +0700

    it works

--- server/src/pages/api/claim.ts
+++ server/src/pages/api/claim.ts
@@ -6,17 +6,20 @@ import { createClient } from "@supabase/supabase-js";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { recoverMessageAddress } from "viem";
 
-await CashServerWasm.initBundled()
-
-const supabase = createClient<Database>("https://nqxmcclvbwhioxcrhtaz.supabase.co", process.env.SUPABASE_KEY!)
-
-const contractZeroHex = "0xabc755011B810fDC31F3504f0F855cadFcb2685A"
-using contractMemory = CashServerWasm.base16_decode_mixed(contractZeroHex.slice(2))
+const supabase = createClient<Database>("https://vqceovbkcavejkqyqbqd.supabase.co", process.env.SUPABASE_KEY!)
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const headers = new Headers({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "POST", "Access-Control-Allow-Headers": "Content-Type" })
+
+  if (req.method === "OPTIONS")
+    return void res.status(200).setHeaders(headers).end()
+  if (req.method !== "POST")
+    return void res.status(405).setHeaders(headers).end()
+
+  const contractZeroHex = "0xabc755011B810fDC31F3504f0F855cadFcb2685A"
   const nonceZeroHex = z.string().asOrThrow(req.body.nonceZeroHex).toLowerCase()
   const secretsZeroHex = z.string().asOrThrow(req.body.secretsZeroHex).toLowerCase()
   const signatureZeroHex = z.string().asOrThrow(req.body.signatureZeroHex).toLowerCase()
@@ -31,16 +34,28 @@ export default async function handler(
     .eq("receiver", receiverZeroHex)
 
   if (error != null)
-    throw new Error("Database error")
+    throw new Error("Database error", { cause: error.message })
 
   if (data.length > 0)
     throw new Error("Nonce replayed")
 
-  using nonceMemory = CashServerWasm.base16_decode_mixed(nonceZeroHex.slice(2))
-  using receiverMemory = CashServerWasm.base16_decode_mixed(receiverZeroHex.slice(2))
-  using secretsMemory = CashServerWasm.base16_decode_mixed(secretsZeroHex.slice(2))
+  await CashServerWasm.initBundled()
+
+  const contractBase16 = contractZeroHex.slice(2).padStart(64, "0")
+  using contractMemory = CashServerWasm.base16_decode_mixed(contractBase16)
+
+  const nonceBase16 = nonceZeroHex.slice(2).padStart(64, "0")
+  using nonceMemory = CashServerWasm.base16_decode_mixed(nonceBase16)
+
+  const receiverBase16 = receiverZeroHex.slice(2).padStart(64, "0")
+  using receiverMemory = CashServerWasm.base16_decode_mixed(receiverBase16)
+
+  console.log({ contractBase16, nonceBase16, receiverBase16 })
 
   using mixinWasm = new CashServerWasm.NetworkMixin(contractMemory, receiverMemory, nonceMemory)
+
+  const secretsBase16 = secretsZeroHex.slice(2)
+  using secretsMemory = CashServerWasm.base16_decode_mixed(secretsBase16)
 
   using valueMemory = mixinWasm.verify_secrets(secretsMemory)
   const valueRawHex = CashServerWasm.base16_encode_lower(valueMemory)
@@ -62,6 +77,6 @@ export default async function handler(
     if (error != null)
       throw new Error("Database error")
 
-    res.status(200).json(valueZeroHex);
+    res.status(200).setHeaders(headers).json(valueZeroHex);
   }
 }
