ea6f43259ea0e5d9b33c5d93d6d1ab76c0647301
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-03-22 12:48:02 +0700

    d

--- client/src/mods/page/index.tsx
+++ client/src/mods/page/index.tsx
@@ -1,5 +1,3 @@
-import { AnchorChip } from "@/libs/ui/anchors";
-import { Ascii } from "@/mods/ascii";
 import { useLocaleContext } from "@/mods/locale/mods/context";
 import { NetWorker } from "@hazae41/networker";
 import Head from "next/head";
@@ -36,7 +34,7 @@ export function Page() {
     const headers = { "Content-Type": "application/json" }
     const body = JSON.stringify({ nonceZeroHex, secretsZeroHex, signatureZeroHex })
 
-    const response = await fetch("http://localhost:3001/api/claim", { method: "POST", headers, body })
+    const response = await fetch("https://api.cash.brume.money/api/claim", { method: "POST", headers, body })
 
     if (!response.ok)
       throw new Error("Claim failed")
@@ -57,71 +55,12 @@ export function Page() {
     </Head>
     <div className="grow flex flex-col w-full m-auto max-w-6xl">
       <div className="h-[100dvh] flex flex-col p-8">
-        <div className="h-[max(24rem,100dvh_-_16rem)] flex flex-col justify-center items-center">
-          <div className="text-default-contrast whitespace-pre-wrap font-[monospace] text-[min(1vw,8px)] leading-[min(1vw,8px)]"
-            dir="ltr">
-            {closed ? Ascii.closed : Ascii.normal}
-          </div>
-        </div>
-        <div className="h-16 grow shrink-0" />
         <div className="font-medium text-6xl">
-          Cash
+          Brume Cash
         </div>
         <div className="h-2 shrink-0" />
         <div className="text-default-contrast text-2xl">
           PoW-based peer-to-peer payment system
-        </div>
-        <div className="h-4 shrink-0" />
-        <div className="flex flex-wrap gap-2">
-          <AnchorChip
-            href="https://wallet.brume.money"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            Wallet
-          </AnchorChip>
-          <AnchorChip
-            href="https://dexscreener.com/ethereum/0xD0EbFe04Adb5Ef449Ec5874e450810501DC53ED5"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            $BRUME
-          </AnchorChip>
-          <AnchorChip
-            href="https://x.com/BrumeProject"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            X.com
-          </AnchorChip>
-          <AnchorChip
-            href="https://discord.gg/KVEPWfN9jK"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            Discord
-          </AnchorChip>
-          <AnchorChip
-            href="https://juicebox.money/v2/p/748?tabid=about"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            Juicebox
-          </AnchorChip>
-          <AnchorChip
-            href="https://github.com/brumeproject"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            GitHub
-          </AnchorChip>
-          <AnchorChip
-            href="https://www.tldraw.com/s/v2_c_WZN9Q33cGQyF_RdbXqLUt"
-            rel="noreferrer"
-            target="_blank"
-            dir="ltr">
-            Tldraw
-          </AnchorChip>
         </div>
       </div>
     </div>
