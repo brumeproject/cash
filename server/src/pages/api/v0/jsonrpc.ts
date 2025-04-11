68cb99d03d4adf5ebf08b6e492aff6946da0efa2
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-04-11 21:49:01 +0700

    d

---
+++ server/src/pages/api/v0/jsonrpc.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { z } from "@hazae41/gardien";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function jsonrpc(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const headers = new Headers({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "POST", "Access-Control-Allow-Headers": "Content-Type" })
+
+  if (req.method === "OPTIONS")
+    return void res.status(200).setHeaders(headers).end()
+  if (req.method !== "POST")
+    return void res.status(405).setHeaders(headers).end()
+
+  const id = z.unknown().asOrThrow(req.body.id)
+  const method = z.string().asOrThrow(req.body.method)
+  const params = z.unknown().asOrThrow(req.body.params)
+
+  if (method === "eth_chainId") {
+    const data = { jsonrpc: "2.0", id, result: "0x6272756d65" }
+    return void res.status(200).setHeaders(headers).json(data);
+  }
+
+  // res.status(200).setHeaders(headers).json(data);
+}
